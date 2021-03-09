92e613a612523c26fc26d65b0ee4889e19978038
Blackbird <niklas@netzvogt.de>
2021-03-09 11:06:28 +0100

    .

--- Pc_PartPicker/PCcase.xaml.cs
+++ Pc_PartPicker/PCcase.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Pc_PartPicker
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for PCcase.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class PCcase : Window
     {
-        public MainWindow()
+        public PCcase()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Komponentenauswahl komponenten = new Komponentenauswahl();
-            komponenten.ShowDialog();
         }
     }
 }
