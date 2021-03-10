82b7d5246b825782a7d10754bf78a104b08eadf7
CodingKes <patrick_kestler@web.de>
2021-03-10 11:55:43 +0100

    up

--- Pc_PartPicker/Build.xaml.cs
+++ Pc_PartPicker/Build.xaml.cs
@@ -20,9 +20,24 @@ namespace Pc_PartPicker
     /// </summary>
     public partial class Build : Window
     {
+        private const int GWL_STYLE = -16;
+        private const int WS_SYSMENU = 0x80000;
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        void ToolWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Code to remove close box from window
+            var hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
+        }
+
         public Build()
         {
             InitializeComponent();
+            Loaded += ToolWindow_Loaded;
             DataTable TabData = new DataTable();
 
             TabData.Columns.Add("id");
