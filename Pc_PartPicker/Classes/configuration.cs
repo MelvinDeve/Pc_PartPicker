e9f2f8c3f580009705b51c5864aab1bd455232fa
joka043 <jonas.kasnitz@gmail.com>
2021-03-11 20:44:53 +0100

    kuhles update lol

--- Pc_PartPicker/Classes/configuration.cs
+++ Pc_PartPicker/Classes/configuration.cs
@@ -9,6 +9,8 @@ namespace Pc_PartPicker
 
     static class configuration
     {
+
+        static configuration() { }
         public static Case pcCase {get; set;}
         public static CPU cpu { get; set; }
         public static CPU_Cooler cpuCooler { get; set; }
@@ -18,5 +20,44 @@ namespace Pc_PartPicker
         public static Motherboard motherboard { get; set; }
         public static Psu psu { get; set; }
         public static List<Storage> storage { get; set; }
+
+    }
+
+    public class configWrite
+    {
+
+        public configWrite()
+        {
+            pcCase = configuration.pcCase;
+            cpu = configuration.cpu;
+            cpuCooler = configuration.cpuCooler;
+            gpu = configuration.gpu;
+            memory = configuration.memory;
+            motherboard = configuration.motherboard;
+            psu = configuration.psu;
+            storage = configuration.storage;
+        }
+        Case pcCase { get; set; }
+        CPU cpu { get; set; }
+        CPU_Cooler cpuCooler { get; set; }
+        Gpu gpu { get; set; }
+
+        List<Memory> memory { get; set; }
+        Motherboard motherboard { get; set; }
+        Psu psu { get; set; }
+        List<Storage> storage { get; set; }
+
+        public void configRead()
+            {
+            configuration.pcCase = pcCase;
+            configuration.cpu = cpu;
+            configuration.cpuCooler = cpuCooler;
+            configuration.gpu = gpu;
+            configuration.memory = memory;
+            configuration.motherboard = motherboard;
+            configuration.psu = psu;
+            configuration.storage = storage;
+            }
+
     }
 }
--- Pc_PartPicker/XMLWrite.cs
+++ Pc_PartPicker/XMLWrite.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Pc_PartPicker
 {
-    class Class1
+    public class XMLWrite
     {
+
+        public static void WriteXML()
+        {
+
+            configWrite conWrite = new configWrite();
+            System.Xml.Serialization.XmlSerializer writer =
+            new System.Xml.Serialization.XmlSerializer(typeof(configWrite));
+
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//Partlist.xml";
+            System.IO.FileStream file = System.IO.File.Create(path);
+
+            writer.Serialize(file, conWrite);
+            file.Close();
+        }
+
+        public void ReadXML()
+        {
+            configWrite conWrite = new configWrite();
+            System.Xml.Serialization.XmlSerializer reader =
+                new System.Xml.Serialization.XmlSerializer(typeof(configWrite));
+            System.IO.StreamReader file = new System.IO.StreamReader(
+                @"c:\temp\Partlist.xml");
+            conWrite = (configWrite)reader.Deserialize(file);
+            file.Close();
+            conWrite.configRead();
+        }
     }
 }
