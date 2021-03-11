53463c91dbbf862f9f64ce5ca9282eeced3818ab
CodingKes <patrick_kestler@web.de>
2021-03-11 11:30:53 +0100

    backend klassen

---
+++ Pc_PartPicker/Classes/Gpu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pc_PartPicker.Classes
+{
+    class Gpu
+    {
+        private String chipSet { get; }
+        private String gpuMemory { get; }
+        private int tdp { get; }
+
+
+        public Gpu(String chipSet, String gpuMemory, String tdp)
+        {
+            this.chipSet = chipSet;
+            this.gpuMemory = gpuMemory;
+            this.tdp = Int32.Parse(tdp);
+        }
+    }
+}
---
+++ Pc_PartPicker/Classes/Memory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pc_PartPicker.Classes
+{
+    class Memory
+    {
+        private String speed { get; }
+        private int modules { get; }
+        private String capacity { get; }
+
+
+
+        public Memory(String speed, String modules, String capacity)
+        {
+            this.speed = speed;
+            this.modules = Int32.Parse(modules);
+            this.capacity = capacity;
+        }
+    }
+}
