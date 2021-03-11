a4b69cbac3cfa6ed00757488d42a1f802ac2b3e5
Melvin <melvin@vertech.de>
2021-03-12 00:08:01 +0100

    asdyc<

--- Pc_PartPicker/Build.xaml.cs
+++ Pc_PartPicker/Build.xaml.cs
@@ -45,6 +45,33 @@ namespace Pc_PartPicker
             bitmap.UriSource = new Uri(@"C:\\Users\\melvi\\source\\PCPartPicker\\Pc_PartPicker\\Pc_PartPicker\\images\\CPU_R5_3600.jpg", UriKind.Absolute);
             bitmap.EndInit();
 
+            Image img = new Image();
+            img.Source = bitmap;
+
+            var column = new DataGridTemplateColumn();
+            column.CanUserResize = false;
+            column.CanUserReorder = false;
+            column.Header = "";
+            column.MaxWidth = 40;
+
+            var imga = new FrameworkElementFactory(typeof(Image));
+
+            imga.SetValue(Image.SourceProperty, img.Source);
+
+            var template = new DataTemplate();
+            template.VisualTree = imga;
+
+            column.CellTemplate = template;
+
+            TableItems.Columns.Add(column);
+
+            TabData.Columns.Add("fdsg");
+            TabData.Columns.Add("image");
+            TabData.Columns.Add("Name1");
+            TabData.Columns.Add("Name2");
+            TabData.Rows.Add(new object[] { 123, "test", "Foo", "Bar" });
+            TableItems.DataContext = TabData.DefaultView;
+
             /*
             
             
@@ -52,55 +79,44 @@ namespace Pc_PartPicker
             FrameworkElementFactory dgtcct = new FrameworkElementFactory(typeof(DataGridTemplateColumn.CellTemplate));
             dgtc.AppendChild(dgtcct);
             */
-            FrameworkElementFactory image = new FrameworkElementFactory(typeof(Image));
-            image.SetBinding(Image.SourceProperty, new Binding(@"C:\\Users\\melvi\\source\\PCPartPicker\\Pc_PartPicker\\Pc_PartPicker\\images\\CPU_R5_3600.jpg"));
-            DataTemplate dt = new DataTemplate();
-            Image img = new Image();
-            img.Source = bitmap;
+            //FrameworkElementFactory image = new FrameworkElementFactory(typeof(Image));
+            //image.SetBinding(Image.SourceProperty, new Binding(@"C:\\Users\\melvi\\source\\PCPartPicker\\Pc_PartPicker\\Pc_PartPicker\\images\\CPU_R5_3600.jpg"));
+            //DataTemplate dt = new DataTemplate();
 
-            DataColumn col = new DataColumn();
-            col.DataType = typeof(Image);
 
+            //DataColumn col = new DataColumn();
+            //col.DataType = typeof(Image);
 
 
 
-            var column = new DataGridTemplateColumn();
-            column.CanUserResize = false;
-            column.CanUserReorder = false;
-            column.Header = "";
+
+
+
 
             //Create FrameworkElementFactory
-            var imga = new FrameworkElementFactory(typeof(Image));
-            //Create binding
-            Binding binding = new Binding("img");
 
-           
+            //Create binding
+            //Binding binding = new Binding("img");
+
+
 
             //Set the converter
             //binding.Converter = new ImageSourceConverter();
             //imga.SetBinding(Image.SourceProperty, binding);
-            imga.SetValue(Image.SourceProperty, img.Source);
+
+
 
             //Create data template
-            var template = new DataTemplate();
-            template.VisualTree = imga;
-            column.CellTemplate = template;
-
-            TableItems.Columns.Add(column);
 
 
 
 
 
-            TabData.Columns.Add("fdsg");
-            TabData.Columns.Add("image", typeof(DataTemplate));
-            TabData.Columns.Add("Name1");
-            TabData.Columns.Add("Name2");
-            TabData.Rows.Add(new object[] { 123, template, "Foo", "Bar" });
-            TableItems.DataContext = TabData.DefaultView;
-            
 
-            
+
+
+
+
 
         }
 
